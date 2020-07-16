54e12cfa0a9ac6b51b1fb4b8167547a51a321b30
jripke74 <jripke74@gmail.com>
2020-07-16 13:41:12 -0700

    add event listener to slots

--- modal.js
+++ modal.js
@@ -75,7 +75,11 @@ class Modal extends HTMLElement {
           <button>Okay</button>
         </section>
       </div>
-    `
+    `;
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
+      console.dir(slots[1].assignedNodes());
+    });
   }
 
   // attributeChangedCallback(name, oldValue, newValue) {
