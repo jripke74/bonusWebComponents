2ec48ed759b821faf772338dbbd3427a33c904f5
jripke74 <jripke74@gmail.com>
2020-07-16 09:40:45 -0700

    add observer for text attribute

--- tooltip.js
+++ tooltip.js
@@ -7,14 +7,29 @@ class Tooltip extends HTMLElement {
     this.shadowRoot.innerHTML = `
       <style>
         div {
+          font-weight: normal;
           background-color: black;
           color: white;
           position: absolute;
+          top: 1.5rem;
+          left: 0.75rem;
           z-index: 10;
+          padding: 0.15rem;
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
         }
 
         :host(.important) {
           background-color: var(--color-primary, #ccc);
+          padding: 0.15rem;
+        }
+
+        :host-context(p) {
+          font-weight: bold;
+        }
+
+        .hightlight {
+          background-color: red;
         }
 
         ::slotted(.highlight) {
@@ -24,9 +39,9 @@ class Tooltip extends HTMLElement {
         .icon {
           background: black;
           color: white;
-          padding; 0.15rem 0.5rem;
+          padding: 0.15rem 0.5rem;
           text-align: center;
-          border-radius: 60%;
+          border-radius: 50%;
         }
       </style>
       <slot>Some default</slot>
@@ -43,6 +58,19 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = 'relative';
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text'];
   }
 
   _showTooltip() {
